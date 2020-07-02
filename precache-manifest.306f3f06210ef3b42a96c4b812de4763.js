67df9e44ec5f2ddd9891a3100c6b5775655533d0
HyunM <wisek0226@gmail.com>
2020-07-02 10:30:37 -0700

    Updates

---
+++ precache-manifest.306f3f06210ef3b42a96c4b812de4763.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893f2c6fbb47a437efe5b32f4a9993c2",
+    "url": "/multi-dictionary/index.html"
+  },
+  {
+    "revision": "72c24e7a000b540690ad",
+    "url": "/multi-dictionary/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "920ad22bf7c1d16465eb",
+    "url": "/multi-dictionary/static/css/main.199bdbb6.chunk.css"
+  },
+  {
+    "revision": "72c24e7a000b540690ad",
+    "url": "/multi-dictionary/static/js/2.863e17de.chunk.js"
+  },
+  {
+    "revision": "3ae53551bb61526a017fad7619b64b1b",
+    "url": "/multi-dictionary/static/js/2.863e17de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920ad22bf7c1d16465eb",
+    "url": "/multi-dictionary/static/js/main.52744edb.chunk.js"
+  },
+  {
+    "revision": "4f2e9a204fcb6d0c3263",
+    "url": "/multi-dictionary/static/js/runtime-main.b8477dda.js"
+  }
+]);
